0d222abe81135fa7788911bc556b6819eba09af0
Andrew Regner <andrew@aregner.com>
2011-12-06 16:58:58 -0600

    rsync flags, python bug fixes

--- setup.user.d/20-python_local.sh
+++ setup.user.d/20-python_local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # local system's python information
-python_major=$(python -c 'import sys; print sys.version_info.major')
-python_minor=$(python -c 'import sys; print sys.version_info.minor')
+python_major=$(python -c 'import sys; print sys.version_info[0]')
+python_minor=$(python -c 'import sys; print sys.version_info[1]')
 python_site=$(python -c 'import site; print site.USER_SITE')
 
 export python_major python_minor python_site
@@ -13,4 +13,4 @@ export python_major python_minor python_site
 # copy updated files to the site dir
 cd $basedir/filesystem
 
-rsync -ca "home/username/.local/lib/python2.x/*" $python_site/
+rsync -ctpr home/username/.local/lib/python2.x/* $python_site/
