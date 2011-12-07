05f7fdc8f674f964db45ed4e707cb7d327f1ea89
Andrew Regner <andrew@aregner.com>
2011-12-07 16:23:32 -0600

    create general folder for user scripts to write to.  use that for the private tarball and key.

---
+++ setup.system.d/02-basedir-for-user.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# a adrtools folder we can write to as the user
+mkdir $basedir/user
+chown $username $basedir/user
--- setup.user.d/10-private.sh
+++ setup.user.d/10-private.sh
@@ -6,8 +6,10 @@ private_port=22
 # must be a bzip2 compressed tarball
 private_file=.adrtools_private.tar.bz2
 
-keyfile=$basedir/private_access_key
+keyfile=$basedir/user/private_access_key
+privatefilestore=$basedir/user/private.tar.bz2
 
+# generate a special key file, if needed
 generate_key() {
     rm -f $keyfile 2>/dev/null
     ssh-keygen -C "auth updates" -N "" -f $keyfile > /dev/null
@@ -15,24 +17,28 @@ generate_key() {
     cat $keyfile.pub | ssh -p $private_port -l $private_user $private_host "sh -c 'mkdir ~/.ssh ; tee -a ~/.ssh/authorized_keys'"
 }
 
+[[ -f $keyfile ]] || generate_key
+
 ssh_config="Host $private_host\nUser $private_user\nPort $private_port\nIdentityFile $keyfile"
 
 # check for ssh config based on what we want
 # (assume that it is all set for updates)
-if [[ -z $update && -f ~/.ssh/config ]]; then
+if [[ -z $update && -f $home/.ssh/config ]]; then
     echo "WARNING: existing ~/.ssh/config detected!  Will not try to alter it with" >&2
     echo "the host and port settings we have.  You should do this manually:" >&2
     echo -e "\nExample:\n$ssh_config" >&2
-else
-    echo -e $ssh_config > ~/.ssh/config
+elif [[ -z $update ]]; then
+    mkdir $home/.ssh 2>/dev/null
+    echo -e $ssh_config > $home/.ssh/config
 fi
 
-private_stat=$(stat -c '%s %y' $basedir/private.tar.bz2 2>/dev/null)
+# check if the file was changed
+private_stat="$(stat -c '%s %y' $privatefilestore 2>/dev/null)"
 
-rsync -tpr $private_host:$private_file $basedir/private.tar.bz2
+rsync -tpr $private_user@$private_host:$private_file $privatefilestore
 
 # private file was changed, extract it
-if [[ "$()" != $private_stat ]]; then
+if [[ "$(stat -c '%s %y' $privatefilestore)" != $private_stat ]]; then
     cd $home
-    tar xvjf $basedir/private.tar.bz2
+    tar xvjf $privatefilestore
 fi
